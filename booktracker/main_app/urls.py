6c27fac05f426517d406d83b1bda179d0832fe4a
KhalilKhunji <khalilkhunji@gmail.com>
2024-09-01 16:06:15 +0300

    book detail page

--- booktracker/main_app/urls.py
+++ booktracker/main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('books/', views.book_index, name='book-index')
+    path('books/', views.book_index, name='book-index'),
+    path('books/<int:book_id>/', views.book_detail, name='book-detail')
 ]
