bc56dc0d8be5ab3bca0de933b57a6049a7b51f7b
KhalilKhunji <khalilkhunji@gmail.com>
2024-09-01 14:16:19 +0300

    created book model

--- booktracker/main_app/models.py
+++ booktracker/main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from isbn_field import ISBNField
+from django.urls import reverse
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=1000)
+    author = models.CharField(max_length=1000)
+    date_of_publication = models.DateField('Publication Date')
+    ISBN = ISBNField()
+
+    def __str__(self):
+        return f'{self.title} by {self.author}'
+    
+    def get_absolute_url(self):
+        return reverse("book-detail", kwargs={"pk": self.id})
+
