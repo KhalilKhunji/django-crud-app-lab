d31216e47f1612762c0b76effceaa2b58be67fc9
KhalilKhunji <khalilkhunji@gmail.com>
2024-09-01 14:44:14 +0300

    book index page complete

--- booktracker/main_app/views.py
+++ booktracker/main_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from .models import Book
 
 def home(request):
     return render(request, 'home.html')
+
+def book_index(request):
+    books = Book.objects.all()
+    return render(request, 'books/index.html', {'books': books})
